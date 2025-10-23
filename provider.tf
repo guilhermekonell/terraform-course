ebe2618b338983f93af068f6e19de2a6af2ad797
guilhermekonell <gui.ri.konell@hotmail.com>
2025-10-22 23:22:12 -0300

    feat: add s3 backend / add vpc configurations

--- provider.tf
+++ provider.tf
@@ -1,12 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.17.0"
     }
+  }
+  backend "s3" {
+    bucket = "378142340425-comunidadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
