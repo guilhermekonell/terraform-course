b6d6bd90c01c033d99c4ab8e2f8d6db6b9c338d8
guilhermekonell <gui.ri.konell@hotmail.com>
2025-10-29 21:39:19 -0300

    feat: added controller module

---
+++ modules/aws-load-balancer-controller/helm.tf
@@ -0,0 +1,28 @@
+resource "helm_release" "eks_helm_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.14.1"
+  namespace  = "kube-system"
+
+  cleanup_on_fail = true
+
+  set = [
+    {
+      name  = "clusterName"
+      value = var.cluster_name
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    },
+    {
+      name  = "vpcId"
+      value = var.vpc_id
+    }
+  ]
+}
